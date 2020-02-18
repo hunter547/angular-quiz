ef43186ef74acfa5de461ab112badbc642daa7b5
Hunter Evanoff <hunter547@gmail.com>
2020-02-18 15:59:51 -0500

    Question form working, displays questions and answer choices.

---
+++ src/app/quiz.model.spec.ts
@@ -0,0 +1,8 @@
+import { Quiz } from './quiz.model';
+import { Question } from './quiz.model';
+
+describe('Quiz', () => {
+  it('should create an instance', () => {
+    expect(new Quiz()).toBeTruthy();
+  });
+});
