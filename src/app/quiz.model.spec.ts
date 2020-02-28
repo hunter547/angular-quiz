839e7d1265cf6bdd6744d0ca1e1d864085352eed
Hunter Evanoff <hunter547@gmail.com>
2020-02-28 17:55:20 -0500

    Auto-generated commit

--- src/app/quiz.model.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Quiz } from './quiz.model';
-import { Question } from './quiz.model';
-
-describe('Quiz', () => {
-  it('should create an instance', () => {
-    expect(new Quiz()).toBeTruthy();
-  });
-});
